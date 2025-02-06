229c5d01725f11de4f55201e6b6b99e33aaa9504
Alexis Haldy <alexishaldy@gmail.com>
2025-02-06 11:18:03 +0100

    Add DelayedCall

---
+++ Scripts/Utility/DelayedCall.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace TryliomUtility
+{
+    /**
+     * Utility class to call a method after a delay (unscaled time)
+     */
+    public static class DelayedCall
+    {
+        public static IEnumerator Call(Action action, float delay)
+        {
+            yield return new WaitForSeconds(delay);
+            action();
+        }
+    }
+}
