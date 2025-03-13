0b3c1f018841697ceeea794aed7e1992f05d14fb
Alexis Haldy <alexishaldy@gmail.com>
2025-03-13 10:16:57 +0100

    Added Serializable object and type

---
+++ Scripts/Utility/SerializableObject.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using UnityEngine;
+
+namespace TryliomUtility
+{
+    [Serializable]
+    public class SerializableObject
+    {
+        [SerializeField] private string _serializedData;
+        [SerializeField] private string _typeName;
+        [SerializeReference] private object _value;
+
+        public object Value
+        {
+            get
+            {
+                if (_value == null && !string.IsNullOrEmpty(_serializedData) && !string.IsNullOrEmpty(_typeName))
+                {
+                    _value = JsonUtility.FromJson(_serializedData, Type.GetType(_typeName));
+                }
+
+                return _value;
+            }
+            set
+            {
+                _value = value;
+                _serializedData = JsonUtility.ToJson(value);
+                _typeName = value.GetType().AssemblyQualifiedName;
+            }
+        }
+
+        public Type ObjectType
+        {
+            get => Type.GetType(_typeName);
+            set => _typeName = value.AssemblyQualifiedName;
+        }
+
+        public SerializableObject()
+        {
+        }
+
+        public SerializableObject(object value)
+        {
+            Value = value;
+        }
+
+        /**
+         * Load the serialized data into the object.
+         */
+        public void LoadSerializedData()
+        {
+            _value = JsonUtility.FromJson(_serializedData, Type.GetType(_typeName));
+        }
+
+        /**
+         * Update the serialized data from the object.
+         */
+        public void UpdateSerializedData()
+        {
+            Value = _value;
+        }
+    }
+}
---
+++ Scripts/Utility/SerializableSystemType.cs
@@ -0,0 +1,138 @@
+﻿// Simple helper class that allows you to serialize System.Type objects.
+// Use it however you like, but crediting or even just contacting the author would be appreciated (Always 
+// nice to see people using your stuff!)
+//
+// Written by Bryan Keiren (http://www.bryankeiren.com)
+
+using UnityEngine;
+
+namespace TryliomUtility
+{
+	[System.Serializable]
+	public class SerializableSystemType
+	{
+		[SerializeField] private string m_Name;
+
+		public string Name
+		{
+			get { return m_Name; }
+		}
+
+		[SerializeField] private string m_AssemblyQualifiedName;
+
+		public string AssemblyQualifiedName
+		{
+			get { return m_AssemblyQualifiedName; }
+		}
+
+		[SerializeField] private string m_AssemblyName;
+
+		public string AssemblyName
+		{
+			get { return m_AssemblyName; }
+		}
+
+		private System.Type m_SystemType;
+
+		public System.Type SystemType
+		{
+			get
+			{
+				if (m_SystemType == null)
+				{
+					GetSystemType();
+				}
+
+				return m_SystemType;
+			}
+		}
+
+		private void GetSystemType()
+		{
+			m_SystemType = System.Type.GetType(m_AssemblyQualifiedName);
+		}
+
+		public SerializableSystemType(System.Type systemType)
+		{
+			m_SystemType = systemType;
+			m_Name = systemType.Name;
+			m_AssemblyQualifiedName = systemType.AssemblyQualifiedName;
+			m_AssemblyName = systemType.Assembly.FullName;
+		}
+
+		public override bool Equals(object obj)
+		{
+			var temp = obj as SerializableSystemType;
+			return (object)temp != null && Equals(temp);
+		}
+
+		public bool Equals(SerializableSystemType @object)
+		{
+			return @object.SystemType == SystemType;
+		}
+
+		public static bool operator ==(SerializableSystemType a, SerializableSystemType b)
+		{
+			// If both are null, or both are the same instance, return true.
+			if (ReferenceEquals(a, b))
+			{
+				return true;
+			}
+
+			// If one is null, but not both, return false.
+			if ((object)a == null || (object)b == null)
+			{
+				return false;
+			}
+
+			return a.Equals(b);
+		}
+
+		public static bool operator !=(SerializableSystemType a, SerializableSystemType b)
+		{
+			return !(a == b);
+		}
+
+		public static bool operator ==(SerializableSystemType a, System.Type b)
+		{
+			// If one is null, but not both, return false.
+			if (((object)a == null) || ((object)b == null))
+			{
+				return false;
+			}
+
+			return a.SystemType == b;
+		}
+
+		public static bool operator !=(SerializableSystemType a, System.Type b)
+		{
+			return !(a == b);
+		}
+
+		public static bool operator ==(System.Type a, SerializableSystemType b)
+		{
+			// If one is null, but not both, return false.
+			if (((object)a == null) || ((object)b == null))
+			{
+				return false;
+			}
+
+			return a == b.SystemType;
+		}
+
+		public static bool operator !=(System.Type a, SerializableSystemType b)
+		{
+			return !(a == b);
+		}
+
+		public static implicit operator SerializableSystemType(System.Type systemType)
+		{
+			return new SerializableSystemType(systemType);
+		}
+
+		public static implicit operator System.Type(SerializableSystemType serializableSystemType)
+		{
+			return serializableSystemType.SystemType;
+		}
+	}
+}
