a3b42cd3ed2588f37e49296feed80fd1b464c0c8
Tryliom <alexishaldy@gmail.com>
2025-04-10 12:57:50 +0200

    Remove guid from SerializableObject
    Because it's isn't working in build

--- Scripts/Utility/SerializableObject.cs
+++ Scripts/Utility/SerializableObject.cs
@@ -1,18 +1,17 @@
 ﻿using System;
 using UnityEngine;
-using UnityEditor;
-using System.Linq;
-using System.Collections.Generic;
 
 namespace TryliomUtility
 {
+    /**
+     * Don't work if the object has a ScriptableObject inside, in that case, find another way with Interface or Abstract class.
+     */
     [Serializable]
     public class SerializableObject
     {
         [SerializeField] private string _serializedData;
         [SerializeField] private string _typeName;
         [SerializeReference] private object _value;
-        [SerializeField] private List<string> _scriptableObjectGUIDs = new List<string>();
 
         public object Value
         {
@@ -21,7 +20,6 @@ namespace TryliomUtility
                 if (_value == null && !string.IsNullOrEmpty(_serializedData) && !string.IsNullOrEmpty(_typeName))
                 {
                     _value = JsonUtility.FromJson(_serializedData, Type.GetType(_typeName));
-                    LoadScriptableObjects();
                 }
 
                 return _value;
@@ -29,11 +27,6 @@ namespace TryliomUtility
             set
             {
                 _value = value;
-                if (value != null)
-                {
-                    _scriptableObjectGUIDs.Clear();
-                    ApplyGUIDsRecursively(value, new HashSet<object>());
-                }
                 _serializedData = JsonUtility.ToJson(value);
                 _typeName = value?.GetType().AssemblyQualifiedName;
             }
@@ -58,7 +51,6 @@ namespace TryliomUtility
         public void LoadSerializedData()
         {
             _value = JsonUtility.FromJson(_serializedData, Type.GetType(_typeName));
-            LoadScriptableObjects();
         }
 
         /**
@@ -67,74 +59,6 @@ namespace TryliomUtility
         public void UpdateSerializedData()
         {
             Value = _value;
-        }
-        
-        private void ApplyGUIDsRecursively(object obj, HashSet<object> visited)
-        {
-            if (obj == null || visited.Contains(obj)) return;
-            visited.Add(obj);
-
-            var fields = obj.GetType().GetFields();
-            foreach (var field in fields)
-            {
-                var fieldValue = field.GetValue(obj);
-                if (fieldValue is ScriptableObject so && so != null)
-                {
-                    var path = AssetDatabase.GetAssetPath(so);
-                    var guid = AssetDatabase.AssetPathToGUID(path);
-                    _scriptableObjectGUIDs.Add(guid);
-                }
-                else if (fieldValue != null && fieldValue.GetType().IsClass && !field.FieldType.IsPrimitive && !field.FieldType.IsValueType)
-                {
-                    ApplyGUIDsRecursively(fieldValue, visited);
-                }
-            }
-        }
-
-        private void LoadScriptableObjects()
-        {
-            if (_value != null)
-            {
-                var guids = new List<string>(_scriptableObjectGUIDs);
-                guids.Reverse();
-                LoadScriptableObjectsRecursively(_value, new Stack<string>(guids), new HashSet<object>());
-            }
-        }
-
-        private void LoadScriptableObjectsRecursively(object obj, Stack<string> guids, HashSet<object> visited)
-        {
-            if (obj == null || guids.Count == 0 || visited.Contains(obj)) return;
-            visited.Add(obj);
-
-            var objectType = obj.GetType();
-            if (!objectType.IsClass || objectType.IsPrimitive || objectType.IsValueType) return;
-
-            var fields = obj.GetType().GetFields();
-            foreach (var field in fields)
-            {
-                if (guids.Count == 0) return;
-
-                var fieldValue = field.GetValue(obj);
-                if (fieldValue == null && typeof(ScriptableObject).IsAssignableFrom(field.FieldType))
-                {
-                    var guid = guids.Pop();
-                    var path = AssetDatabase.GUIDToAssetPath(guid);
-                    if (!string.IsNullOrEmpty(path))
-                    {
-                        var loadedObject = AssetDatabase.LoadAssetAtPath<ScriptableObject>(path);
-                        field.SetValue(obj, loadedObject);
-                        LoadScriptableObjectsRecursively(loadedObject, guids, visited);
-                    }
-                    else
-                    {
-                        Debug.LogError($"Failed to load ScriptableObject with GUID: {guid}");
-                    }
-                }
-                else if (fieldValue != null && fieldValue.GetType().IsClass && !field.FieldType.IsPrimitive && !field.FieldType.IsValueType)
-                {
-                    LoadScriptableObjectsRecursively(fieldValue, guids, visited);
-                }
-            }
         }
     }
 }
